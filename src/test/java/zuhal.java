daf152976421efaf51a40fe823293b4986e3e66f
zuhalsWonderland <72165041+zuhalsWonderland@users.noreply.github.com>
2020-10-26 20:32:50 +0000

    new changes

--- src/test/java/zuhal.java
+++ src/test/java/zuhal.java
@@ -1,5 +1,5 @@
 public class zuhal {
     public static void main(String[] args) {
-        System.out.println("hello. this is a new file");
+        System.out.println("hello. this is a new file.Do you like it.Hello hello");
     }
 }
